5d6995c1e5d3bef146ec39dc41752a06d09f9a59
Zane Selvans <zane.selvans@catalyst.coop>
2022-02-24 11:30:55 -0600

    Switch to using conda environment for tests.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Archive raw PUDL inputs as frictionless data packages on Zenodo."""
 
 from setuptools import setup, find_packages
 
@@ -8,5 +9,18 @@ setup(
     packages=find_packages(),
     author="Catalyst Cooperative",
     description="Zenodo storage interface and scripts",
-    scripts=["./bin/zenodo_store.py"]
+    scripts=["./bin/zenodo_store.py"],
+    python_requires=">=3.8,<3.11",
+    license="MIT",
+    install_requires=[
+        "datapackage<2",
+        "factory_boy<3",
+        "requests<3",
+        "semantic_version<3",
+    ],
+    extras_require={
+        "test": [
+            "pytest>=7",
+        ]
+    }
 )
