4810924acc27c1682c08ccfe20e7c384e65187db
Austen Sharpe <austensharpe@gmail.com>
2022-06-07 16:21:47 -0600

    Add test module for epa_eia_crosswalk archive

---
+++ tests/epa_eia_crosswalk_test.py
@@ -0,0 +1,42 @@
+"""Tests for EPA-EIA Crosswalk file."""
+import random
+
+from datapackage import Package
+from faker import Faker
+
+from pudl_zenodo_storage.frictionless import epa_eia_crosswalk
+
+
+class TestEpaEiaCrosswalkSource:
+    """Ensure we can create proper frictionless datapackage descriptions."""
+
+    def test_file_resource(self, zenodo_url):
+        """Test that file resources are made correctly."""
+        fake = Faker()
+        name = random.choice(["epa_eia_crosswalk.zip"])
+        url = zenodo_url
+        size = random.randint(500000, 800000)
+        md5_hash = fake.md5(raw_output=False)
+
+        fake_resource = {
+            "filename": name,
+            "links": {"download": url},
+            "filesize": size,
+            "checksum": md5_hash,
+        }
+
+        package = epa_eia_crosswalk.datapackager([fake_resource])
+        res = package["resources"][0]
+        print(Package(descriptor=package).errors)
+
+        assert Package(descriptor=package).valid
+        assert res["name"] == name
+        assert res["title"] == name[:-4]
+        assert res["path"] == url
+        assert res["remote_url"] == url
+
+        assert res["mediatype"] == "application/zip"
+        assert res["format"] == "zip"
+
+        assert res["bytes"] == size
+        assert res["hash"] == md5_hash
